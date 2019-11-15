114db07d9ef54440ce3a045ed0cb29271e97939c
Zanhua1999 <skyair886@gmail.com>
2019-11-15 13:14:45 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = "Wat is je voornaam?";
+$b = "wat is je achternaam?";
+
+print $a;
+$line1 = readline("");
+print $b;
+$line2 = readline("");
+
+print_r("jouw naam is: " . $line1 . $line2);
+?>
